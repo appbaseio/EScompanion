50a0cd4ba67abc5f46dcc6234f76b8b89785932a
girishramnani <girishramnani95@gmail.com>
2016-07-15 19:33:45 +0530

    added kibana entry point

---
+++ kibana_entry.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Add kibana as command if needed
+if [[ "$1" == -* ]]; then
+	set -- kibana "$@"
+fi
+
+# Run as user "kibana" if the command is "kibana"
+if [ "$1" = 'kibana' ]; then
+    
+    args=("$@")
+    length="$#"
+    length=$((length-1))
+
+    for i in $(seq 1 "$length");
+    do
+        kibana plugin --install ${args[$i]}
+    done
+
+	if [ "$ELASTICSEARCH_URL" ]; then
+		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
+	fi
+
+	gosu root kibana
+fi
+
